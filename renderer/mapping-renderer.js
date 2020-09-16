44e771581d516d9b8e8f84cc9cef890d283bcf0c
larsvankleef <larsvankleef@outlook.com>
2020-09-16 16:50:20 +0200

    Renturn value instead of a function

--- renderer/mapping-renderer.js
+++ renderer/mapping-renderer.js
@@ -1,14 +1,12 @@
 export default function mappingRenderer(template) {
   return `|<?php
-
-          |return function () {
-          | try {
-          |   return json_decode('${JSON.stringify(template)}', true, 512, JSON_OBJECT_AS_ARRAY);
-          | } catch(\\Exception $e) {
-          |   return new \\WP_error('json decode error', $e->getMessage());
-          | }
-          |};
-
+          |
+          |try {
+          |  return json_decode('${JSON.stringify(template)}', true, 512, JSON_OBJECT_AS_ARRAY);
+          |} catch(\\Exception $e) {
+          |  return new \\WP_error('json decode error', $e->getMessage());
+          |}
+          |
           |?>
-          |`.replace(/^[ \t]*\|/m, '')
+          |`.split(/^[ \t]*\|/m).join('')
 }
