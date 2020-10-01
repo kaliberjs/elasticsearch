7b446ffae4e1144d8cefacadebe483ee5b73416b
larsvankleef <larsvankleef@outlook.com>
2020-10-01 13:33:33 +0200

    Update repeater with nested type

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 module.exports = {
-  text,
-  keyword,
-  integer,
-  long,
-  date,
   completion,
-  searchAsYouType,
+  date,
   flexibleContent,
-  repeater,
+  integer,
+  keyword,
+  long,
+  nested,
   object,
+  repeater,
+  searchAsYouType,
+  text,
 }
 
 function text(props = {}) { return { type: 'text', ...props } }
@@ -19,6 +20,7 @@ function completion(props = {}) { return { type: 'completion', ...props }}
 function searchAsYouType(props = {}) { return { type: 'search_as_you_type', ...props }}
 function date(props = {}) { return { type: 'date', ...props }}
 function object(properties) { return { properties } }
+function nested(properties) { return { type: 'nested', properties }}
 
 function flexibleContent(types) {
   return {
@@ -40,16 +42,5 @@ function flexibleContent(types) {
 }
 
 function repeater(fields) {
-  return {
-    type: 'nested',
-    ...object({
-      items: {
-        type: 'nested',
-        properties: Object.entries(fields).reduce(
-          (result, [name, type]) => ({ ...result, [name]: type }),
-          {}
-        )
-      }
-    })
-  }
+  return nested(fields)
 }
