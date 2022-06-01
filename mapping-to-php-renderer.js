017853f8b78b1f9d3ff4c1fdcd77d7ee07e37377
Harmen Sielcken <hsielcken@kaliber.net>
2022-06-01 16:29:22 +0200

    add mapping-to-php-renderer

--- mapping-to-php-renderer.js
+++ mapping-to-php-renderer.js
@@ -1,3 +1,2 @@
-const renderer = require('./lib/mapping-to-php-renderer')
-
-module.exports = renderer
+Object.defineProperty(exports, "__esModule", { value: true });
+Object.assign(exports, require('./lib/mapping-to-php-renderer.js'));
